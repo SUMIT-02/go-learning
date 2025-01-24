66136984b34790f26c59d2fc5bfa32aee7b2ef57
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-01-24 12:12:44 +0530

    first commit

---
+++ 2_sample_values/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//integer
+	fmt.Println(1)
+	//string
+	fmt.Println("hello golang")
+	//bollen
+	fmt.Println(true)
+	//flot
+	fmt.Println(10.5)
+}
---
+++ 3_variabels/variabels.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var name string = "Golang"
+	//fmt.Println(name)
+	//var name = "Golang"
+	//fmt.Println(name)
+	//shorthand
+	name := "golang"
+	fmt.Println(name)
+}
