5691bf6a08bd9ccd27d5fb484b44ae2f9fb12b2a
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-01-27 12:24:48 +0530

    Loops

---
+++ 5_for/for.go
@@ -0,0 +1,33 @@
+package main
+
+// there is only for loop in go lang
+
+// while loop using for loop
+func main() {
+	//	i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+
+	// 	infinity loop
+
+	// 	}
+	// }
+
+	//infiny loop
+
+	// for {
+	// 	fmt.Println("1")
+	// }
+
+	// for i := 1; i <= 3; i++ {
+	// 	//break
+
+	// 	if i == 2 {
+	// 		continue
+	// 	}
+
+	// 	fmt.Println(i)
+	// }
+
+}
---
+++ 6_if_else/if.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// age := 10
+	// if age >= 18 {
+	// 	fmt.Println("person is adult")
+	// } else {
+	// 	fmt.Println("prson is not adult")
+	// }
+
+	// age := 10
+	// if age >= 18 {
+	// 	fmt.Println("person is adult")
+	// } else if age >= 14 {
+	// 	fmt.Println("person is not adult")
+	// } else {
+	// 	fmt.Println("person is kid")
+	// }
+
+	var role = "admin"
+	var hasaccess = false
+	if role == "admin" && hasaccess {
+		fmt.Println("yes")
+	}
+
+}
