1c838b4850ee1ae664d77ca35fbce418145346cd
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-01-28 23:34:31 +0530

    array

---
+++ 8_array/array.go
@@ -0,0 +1,29 @@
+package main
+
+//specific number of numbers
+//array used at place we alredy know the size
+func main() {
+	// var nums [4]int
+
+	// nums[0] = 1
+	// fmt.Println(nums[0])
+
+	// //array length
+	// //fmt.Println(len(nums))
+	// var name [3]string
+	// name[0] = "golang"
+	// fmt.Println(name)
+
+	//singel line declaration
+	// nums := [3]int{1, 2, 3}
+	// fmt.Println(nums)
+
+	//2d array
+
+	// nums := [2][2]int{{2, 3}, {4, 5}}
+	// fmt.Println(nums)
+	//fixed size array used
+	// memory optimization
+	//constant time
+	//very fast
+}
